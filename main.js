de2e88c49607adefcb82cf5cd7c30396e4423280
Michael Germany <mike@rickgermany.com>
2015-01-26 17:35:17 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// Loop Practice #2
+
+
+
+//#1
+
+var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+
+// for(var i = 0; i < animals.length - 1; i++) {
+// 	console.log(animals[i]);
+// }
+
+
+
+//#2
+
+// for(var i = 0; i <= animals.length - 1; i += 2) {
+//  	console.log(animals[i]);
+//  }
+
+
+
+// #3
