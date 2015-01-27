16cbc3b511e94238abffd8f84de95ad60eb278b6
Michael Germany <mike@rickgermany.com>
2015-01-27 11:47:15 -0700

    final commit

--- main.js
+++ main.js
@@ -21,3 +21,23 @@ var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
 
 
 // #3
+
+// for(var i = animals.length - 1; i >= 0; i--) {
+// console.log(animals[i]);
+// }
+
+// Wanted to use .reverse() but wasn't successful
+// console.log(animals.reverse());
+
+
+
+// #4
+
+// for(var i = 0; i < animals.length; i++) {
+// 	if (i <= 0 || i >= animals.length - 1) {
+// 		console.log(animals[i]);
+// 	} else {
+// 		console.log(animals[i]);
+// 		console.log(animals[i]);
+// 	}
+// }
